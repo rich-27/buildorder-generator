30d44b3cdf534ca65e3599b60946d4be9dde209d
Richard Lewis <rich-27@users.noreply.github.com>
2020-11-19 01:15:26 +0000

    Angular version update

--- src/app/build-order/build-order.component.spec.ts
+++ src/app/build-order/build-order.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BuildOrderComponent } from './build-order.component';
 
@@ -6,7 +6,7 @@ describe('BuildOrderComponent', () => {
   let component: BuildOrderComponent;
   let fixture: ComponentFixture<BuildOrderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BuildOrderComponent ]
     })
--- src/app/full-svg/full-svg.component.spec.ts
+++ src/app/full-svg/full-svg.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FullSvgComponent } from './full-svg.component';
 
@@ -6,7 +6,7 @@ describe('FullSvgComponent', () => {
   let component: FullSvgComponent;
   let fixture: ComponentFixture<FullSvgComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FullSvgComponent ]
     })
