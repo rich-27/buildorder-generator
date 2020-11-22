d5164d22d445126b1671718f7724fcbb3119bbc1
Richard Lewis <rich-27@users.noreply.github.com>
2020-11-22 13:04:55 +0000

    Add ShapeService injection to PointsPipe testbed

--- src/app/pipes/points.pipe.spec.ts
+++ src/app/pipes/points.pipe.spec.ts
@@ -1,8 +1,11 @@
+import { inject } from '@angular/core/testing';
+import { ShapeService } from '@services/shape.service';
 import { PointsPipe } from './points.pipe';
 
+
 describe('PointsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PointsPipe();
+  it('create an instance', inject([ ShapeService ], (shapeService: ShapeService) => {
+    const pipe = new PointsPipe(shapeService);
     expect(pipe).toBeTruthy();
-  });
+  }));
 });
